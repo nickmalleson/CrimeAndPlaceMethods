248f57b7c116f2d8dd626d7cfea2ea94f24d5c70
Nick Malleson <>
2020-08-17 10:30:37 +0100

    initial commit of twitter analysis chapter code

---
+++ Chapter-Twitter/README.md
@@ -0,0 +1,7 @@
+# Social media -- Twitter data
+
+This chapter briefly discusses the use of Twitter data in research and outlines one way of obtaining some data using the Twitter APIs and R. 
+
+The file [twitter_analysis.R](./twitter_analysis.R) contains the code required to collect and map the data.
+
+<img src="./plot.png" alt="Plot of tweets in Washington produced by running the code" />
---
+++ Chapter-Twitter/twitter_analysis.R
@@ -0,0 +1,78 @@
+# This is all adapted from the excellent 'Earth Data Analytics Online' lessons 'Work With Twitter Social Media Data in R - An Introduction'
+# https://www.earthdatascience.org/courses/earth-analytics/get-data-using-apis/intro-to-social-media-text-mining-r/
+# and the documentation for the `rtweet` package
+# https://github.com/ropensci/rtweet
+
+
+# Load twitter library - the rtweet library is recommended now over twitteR
+# LIbrary documentaiton is at: https://cran.r-project.org/web/packages/rtweet/index.html
+library(rtweet)
+
+# Plotting and pipes - tidyverse!
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+# Text mining library
+#library(tidytext)
+
+# Working with JSON
+library(rjson)
+library(jsonlite)
+
+# For plotting tweets
+library(maps)
+
+# For reading the Washington shapefile
+library(rgdal)
+library(sf)
+
+# Set the working directory to be the same directory that this script is stored in. For me this is:
+setwd("/Users/nick/gp/CrimeAndPlaceMethods/Chapter-Twitter")
+
+# whatever name you assigned to your created app
+appname <- ""
+
+# Details provided when you set up the app
+consumer.key <- ""
+consumer.secret <- ""
+access.token <- ""
+access.token.secret <- ""
+
+# create token named "twitter_token"
+twitter_token <- create_token(
+  app = appname,
+  consumer_key = consumer.key,
+  consumer_secret = consumer.secret,
+  access_token = access.token,
+  access_secret = access.token.secret)
+
+### Search for some tweets sent from the US
+#rt <- search_tweets(
+#  "lang:en", geocode = lookup_coords("usa"), n = 1000
+#)
+
+# Search from some tweets 10miles around Washington DC
+rt <- search_tweets(
+  "lang:en", geocode = "38.89511,-77.03637,10mi", n = 10000
+)
+
+# Create lat/lng variables using all available tweet and profile geo-location data
+rt <- lat_lng(rt)
+
+# Drop any lines without coordinates
+rt <- rt[!(is.na(rt$lat) | is.na(rt$lng)),]
+
+# Create a SpatialPoints dataframe for the tweets
+tweets.sp <- SpatialPointsDataFrame(coords=cbind(rt$lng, rt$lat), data=rt, proj4string = CRS("+init=epsg:4326"))
+
+# Read the area boundaries for the study area
+# ** NOTE: you need to unzip the' 'wash_dc_block_groups.zip' first **
+shape <- readOGR(dsn = "../WashingtonDC_Data/wash_dc_block_groups/", layer = "wash_dc_block_groups")
+
+# Project the tweets to the same coordinate reference system to that of the study area
+tweets.sp <- spTransform(tweets.sp, CRS(proj4string(shape)))
+
+# Plot!
+plot(shape)
+points(tweets.sp, pch="+", col="blue", cex=2)
